ea995e4aed70c90b39fafc3f121417da0a879859
Sonashree617 <sonashree617@gmail.com>
2026-03-18 18:32:42 +0530

    fix cors for vercel

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,29 @@
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
+
 import connectDB from "./config/db.js";
+
 import authRoutes from "./routes/authRoutes.js";
 import dishRoutes from "./routes/dishRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 
-
 const app = express();
 
-// middleware
-app.use(cors());
-app.use(express.json());
-
-// connect database
+// ---------------- DATABASE ----------------
 connectDB();
 
+// ---------------- MIDDLEWARE ----------------
+
+app.use(cors({
+  origin: "https://homechef-mern-project-pfye-ctlankcxu-sonas-projects-9fce1112.vercel.app",
+  credentials: true
+}));
+
+app.use(express.json());
 
 // ---------------- ROUTES ----------------
 
@@ -26,12 +32,13 @@ app.use("/api/dishes", dishRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
 
+// ---------------- TEST ROUTE ----------------
 
-// test route
 app.get("/", (req, res) => {
   res.send("HomeChef Marketplace API Running 🚀");
 });
 
+// ---------------- SERVER ----------------
 
 const PORT = process.env.PORT || 5000;
 
