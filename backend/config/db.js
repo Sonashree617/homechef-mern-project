b023ba8df7f7e0a6f7326bfbee230311d6bd0ad2
Sonashree617 <sonashree617@gmail.com>
2026-03-18 22:36:08 +0530

    fix db name

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "homechef"
+    });
     console.log("MongoDB Connected ✅");
     console.log("Connected database:", mongoose.connection.name);
   } catch (error) {
